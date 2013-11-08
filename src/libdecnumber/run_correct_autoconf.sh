82d68429f8310094b222a366519f5d923e00451c
Eric Gallager <egallager@comcast.net>
2013-11-08 13:14:30 -0500

    un-gitignore src/libdecnumber
    Change-Id: Ib311bde998e1aaaa95e2696be728e2df39d7eb97

---
+++ src/libdecnumber/run_correct_autoconf.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "This script assumes that your copy of autoconf264 has suffixed its commands with \"264\" like the MacPorts autoconf264 package does."
+echo "If your autoconf264 is not like this, then this script will probably fail."
+
+set -ex
+
+export AUTOCONF=autoconf264
+export AUTOHEADER=autoheader264
+export AUTOM4TE=autom4te264
+export AUTORECONF=autorecon264
+export AUTOSCAN=autoscan264
+export AUTOUPDATE=autoupdate264
+export IFNAMES=ifnames264
+
+test -x `which autoreconf264` && autoreconf264 -fvi --warnings=all
+
+unset AUTOCONF
+unset AUTOHEADER
+unset AUTOM4TE
+unset AUTORECONF
+unset AUTOSCAN
+unset AUTOUPDATE
+unset IFNAMES
