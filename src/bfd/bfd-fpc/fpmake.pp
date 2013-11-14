bce3a9759c459f69db741c78ee89e84982934654
Eric Gallager <egallager@comcast.net>
2013-11-13 20:08:30 -0500

    copy fpc bindings to gdb and bfd from Fink's fpcsrc package
    and also BFD.pm from Fink's bfd-pm package
    
    Change-Id: I517a6d0e7a4982a6d2b034e1b890f314f937b3c0

---
+++ src/bfd/bfd-fpc/fpmake.pp
@@ -0,0 +1,35 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('bfd');
+{$ifdef ALLPACKAGES}
+    P.Directory:='bfd';
+{$endif ALLPACKAGES}
+    P.Version:='2.6.2';
+    P.Author := 'Library: Cygnus Support, header: by Uli Tessel';
+    P.License := 'Library: GPL2 or later, header: LGPL with modification, ';
+    P.HomepageURL := 'www.freepascal.org';
+    P.Email := '';
+    P.Description := 'Binary File Descriptor library.';
+    P.NeedLibC:= true;
+
+    P.SourcePath.Add('src');
+
+    T:=P.Targets.AddUnit('bfd.pas');
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
---
+++ src/gdb/gdbint-fpc/fpmake.pp
@@ -0,0 +1,48 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('gdbint');
+{$ifdef ALLPACKAGES}
+    P.Directory:='gdbint';
+{$endif ALLPACKAGES}
+    P.Version:='2.6.2';
+    P.Author := 'Library : Cygnus, header: Peter Vreman';
+    P.License := 'Library: GPL2 or later, header: LGPL with modification, ';
+    P.HomepageURL := 'www.freepascal.org';
+    P.Email := '';
+    P.Description := 'Interface to libgdb, the GDB debugger in library format';
+    P.NeedLibC:= true;  // true for headers that indirectly link to libc?
+
+    P.SourcePath.Add('src');
+    P.IncludePath.Add('src');
+    //
+    // NOTE: the gdbver.inc dependancies gives warnings because the makefile.fpc
+    // does a "cp src/gdbver_nogdb.inc src/gdbver.inc" to create it
+
+    T:=P.Targets.AddUnit('gdbcon.pp');
+      with T.Dependencies do
+        begin
+          AddUnit('gdbint');
+        end;
+    T:=P.Targets.AddUnit('gdbint.pp');
+      with T.Dependencies do
+        begin
+          AddInclude('gdbver.inc');
+        end;
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
