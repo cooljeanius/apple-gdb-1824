1b9af44cbb9c3f43d0b4f270df4f4aac631a203f
cooljeanius <egall@gwmail.gwu.edu>
2016-09-26 10:31:47 -0400

    distcleaning, September 26, 2016
    Change-Id: I26e4cd2bd9ee29291e5d1cbe88e1123c8445eefd

--- src/gdb/testsuite/cell69803.c
+++
@@ -1,7 +0,0 @@
-#include <libspe2.h>
-extern spe_program_handle_t cell69803_spu_x;
-int main (void) {
-unsigned int entry = SPE_DEFAULT_ENTRY;
-spe_context_ptr_t ctx = spe_context_create (0, NULL);
-spe_program_load (ctx, &cell69803_spu_x);
-return spe_context_run (ctx, &entry, 0, NULL, NULL, NULL); }
