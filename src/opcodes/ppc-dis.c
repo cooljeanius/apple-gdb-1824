33c6cc41732d6d6d8fca690af5a92914f6454831
cooljeanius <egall@gwmail.gwu.edu>
2017-10-18 10:51:41 -0400

    sorry for all these minor commits in a row, but I have to fix things from compile farm 1 at a time
    Change-Id: I8f576a04356af2efcff044ce6f08030ba211cfc3

--- src/opcodes/ppc-dis.c
+++ src/opcodes/ppc-dis.c
@@ -17,8 +17,9 @@ the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this file; see the file COPYING.  If not, write to the Free
-Software Foundation, 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
+Software Foundation, 51 Franklin St., 5th Floor, Boston, MA 02110-1301, USA */
 
+#include "config.h"
 #include <stdio.h>
 #include "bfd.h"
 #include "sysdep.h"
