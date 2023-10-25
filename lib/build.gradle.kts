5387d91f750ae4543676de1ee99ebadbd5bff916
Eric Gallager <egall@gwmail.gwu.edu>
2023-10-25 03:25:24 -0400

    try turning java codeql back on... again.
    Change-Id: I46170c386733c58e3ac077f271bbd622cb30a35f

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -13,6 +13,11 @@ plugins {
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
+    mavenLocal {
+        content {
+            includeGroup("com.jcraft")
+        }
+    }
 }
 
 dependencies {
@@ -31,7 +36,7 @@ dependencies {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(7))
+        languageVersion.set(JavaLanguageVersion.of(8))
     }
 }
 
